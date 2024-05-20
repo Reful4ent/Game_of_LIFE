7a237faab7e96d26a9f781e3350459f8e6914c59
Reful4ent <dima2323fr3@gmail.com>
2024-05-20 13:30:06 +0700

    Added Interface for GameManager

--- Game_of_LIFE/Model/GameManagerFolder/GameManager.cs
+++ Game_of_LIFE/Model/GameManagerFolder/GameManager.cs
@@ -2,63 +2,60 @@
 
 namespace Game_of_LIFE.Model;
 
-public class GameManager
+public class GameManager : IGameManager
 {
     private IField fieldStartSet;
     private int timeDelay = 1000;
-
     public int TimeDelay
     {
         get => timeDelay;
         private set => timeDelay = value;
     }
-
+    public IField FieldStartSet
+    {
+        get => fieldStartSet;
+        private set => fieldStartSet = value;
+    }
     public event Action<IField>? FieldRefreshed;
-
     public GameManager(IField fieldStartSet)
     {
         if (fieldStartSet == null)
             throw new NullReferenceException("Поле должно быть задано!");
-        this.fieldStartSet = fieldStartSet;
+        FieldStartSet = fieldStartSet;
     }
-
     public bool ChangeField(IField fieldStartSet)
     {
         if (fieldStartSet == null)
             return false;
-        this.fieldStartSet = fieldStartSet;
+        FieldStartSet = fieldStartSet;
         return true;
     }
-
     public bool Start(bool fieldType)
     {
         while (true)
         {
-            fieldStartSet.PrintCellField();
-            if (fieldStartSet.Step(fieldType))
+            FieldStartSet.PrintCellField();
+            if (FieldStartSet.Step(fieldType))
                 break;
             Thread.Sleep(2000);
             Console.Clear();
         }
         return true;
     }
-
     public async Task<bool> StartAsync(bool fieldType)
     {
         await Task.Run(async () =>
         {
             while (true)
             {
-                fieldStartSet.PrintCellField();
-                if (fieldStartSet.Step(fieldType))
+                if (FieldStartSet.Step(fieldType))
                     break;
-                await Task.Delay(timeDelay);
-                Console.Clear();
+                FieldRefreshed?.Invoke(FieldStartSet);
+                await Task.Delay(TimeDelay);
             }
         });
         return true;
     }
-
     public bool ChangeSpeed(int timeDelay)
     {
         if (timeDelay < 200 || timeDelay > 10000)
--- Game_of_LIFE/Model/GameManagerFolder/IGameManager.cs
+++ Game_of_LIFE/Model/GameManagerFolder/IGameManager.cs
@@ -1,6 +1,13 @@
-﻿namespace Game_of_LIFE.Model;
+﻿using Game_of_LIFE.Model.Interfaces;
+namespace Game_of_LIFE.Model;
 
-public class IGameManager
+public interface IGameManager
 {
-    
+    public int TimeDelay { get; }
+    public IField FieldStartSet { get; }
+    public event Action<IField>? FieldRefreshed;
+    public bool ChangeField(IField fieldStartSet);
+    public bool Start(bool fieldType);
+    public Task<bool> StartAsync(bool fieldType);
+    public bool ChangeSpeed(int timeDelay);
 }
