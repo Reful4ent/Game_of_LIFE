76017d4776e4e70f169cc59e0905f235365c767a
Reful4ent <dima2323fr3@gmail.com>
2024-05-21 23:22:20 +0700

    Added Stop and clearing field in GameManager, fixed Step method (changed Cycle for a NonCycle)  in Field

--- Game_of_LIFE/Program.cs
+++ Game_of_LIFE/Program.cs
@@ -2,23 +2,21 @@
 using Game_of_LIFE.Model.Interfaces;
 using Microsoft.VisualBasic;
 
-IField field = Field.Instance(11,6);
+IField field = Field.Instance(9,9);
 
 
 
 
 int[,] c = {
-    {0,0,0,1,0,0},
-    {0,0,0,0,1,0},
-    {0,0,1,1,1,0},
-    {0,0,0,0,0,0},
-    {0,0,0,0,0,0},
-    {0,0,0,0,0,0},
-    {0,0,0,0,0,0},
-    {0,0,0,0,0,0},
-    {0,0,0,0,0,0},
-    {1,1,1,0,0,0},
-    {0,0,0,0,0,0}
+    {0,0,0,0,0,0,0,0,0},
+    {0,0,0,0,0,0,0,0,0},
+    {0,0,0,0,0,0,0,0,0},
+    {0,0,0,0,1,0,0,0,0},
+    {0,0,0,1,1,1,0,0,0},
+    {0,0,0,0,1,0,0,0,0},
+    {0,0,0,0,0,0,0,0,0},
+    {0,0,0,0,0,0,0,0,0},
+    {0,0,0,0,0,0,0,0,0},
 };
 
 field.SetCellField(c);
@@ -28,7 +26,7 @@ while (true)
     field.PrintCellField();
     if (field.Step(false))
         break;
-    Thread.Sleep(2000);
+    Thread.Sleep(300);
     Console.Clear();
 }
 
